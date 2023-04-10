5464c76323bee7943b419c2e4e44e7d97ce2424d
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-04-10 13:40:28 -0400

    yes

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "DirtyCowKit",
-            targets: ["SwiftFrontend", "Exploit"]),
+            targets: ["MDC", "Exploit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftFrontend",
+            name: "MDC",
             dependencies: ["Exploit"]),
         .target(
             name: "Exploit",
--- Sources/MDC/MDC.swift
+++ Sources/MDC/MDC.swift
@@ -4,7 +4,7 @@ extension String: LocalizedError {
     public var errorDescription: String? { return self }
 }
 
-public enum DirtyCowKit {
+public enum MDC {
     public static var isMDCSafe: Bool = true
     
     public static func restartBackboard() {
