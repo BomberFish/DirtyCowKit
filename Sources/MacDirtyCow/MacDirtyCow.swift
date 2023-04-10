5f30c63dd998e47cc72d11d0bd6fb26ab882960f
BomberFish <87151697+BomberFish@users.noreply.github.com>
2023-04-10 14:24:35 -0400

    add togglecatalogcorruption

--- Sources/MacDirtyCow/MacDirtyCow.swift
+++ Sources/MacDirtyCow/MacDirtyCow.swift
@@ -102,4 +102,39 @@ public enum MacDirtyCow {
             }
         }
     }
+    
+    public static func toggleCatalogCorruption(at path: String, corrupt: Bool) throws {
+        let fd = open(path, O_RDONLY | O_CLOEXEC)
+        guard fd != -1 else { throw "Could not open target file" }
+        defer { close(fd) }
+        
+        let buffer = UnsafeMutablePointer<Int>.allocate(capacity: 0x4000)
+        let n = read(fd, buffer, 0x4000)
+        var byteArray = [UInt8](Data(bytes: buffer, count: n))
+        
+        let treeBytes: [UInt8] = [0, 0, 0, 0, 0x74, 0x72, 0x65, 0x65, 0, 0, 0]
+        let corruptBytes: [UInt8] = [67, 111, 114, 114, 117, 112, 116, 84, 104, 105, 76]
+        
+        let findBytes = corrupt ? treeBytes : corruptBytes
+        let replaceBytes = corrupt ? corruptBytes : treeBytes
+        
+        var startIndex = 0
+        while startIndex <= byteArray.count - findBytes.count {
+            let endIndex = startIndex + findBytes.count
+            let subArray = Array(byteArray[startIndex..<endIndex])
+            
+            if subArray == findBytes {
+                byteArray.replaceSubrange(startIndex..<endIndex, with: replaceBytes)
+                startIndex += replaceBytes.count
+            } else {
+                startIndex += 1
+            }
+        }
+        
+        let overwriteSucceeded = byteArray.withUnsafeBytes { dataChunkBytes in
+            unaligned_copy_switch_race(
+                fd, 0, dataChunkBytes.baseAddress, dataChunkBytes.count)
+        }
+        print("overwriteSucceeded = \(overwriteSucceeded)")
+    }
 }
